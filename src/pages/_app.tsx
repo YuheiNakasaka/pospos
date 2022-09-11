301af1d86c16a0d6afe4fdf66adf3099026865ac
YuheiNakasaka <yuhei.nakasaka@gmail.com>
2022-09-11 10:19:17 +0900

    Reset

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,7 @@
 import type { AppProps } from 'next/app'
 
 import '../style.css'
-import '../App.css'
-import { DbProvider } from '../components/contexts/db'
 
 export default function MyApp({ Component, pageProps }: AppProps) {
-  return (
-    <DbProvider>
-      <Component {...pageProps} />
-    </DbProvider>
-  )
+  return <Component {...pageProps} />
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,77 +1,7 @@
 import { useState } from 'react'
-import { invoke } from '@tauri-apps/api/tauri'
-import { useDb } from '../components/contexts/db'
 
 function App() {
-  const [host, setHost] = useState('localhost')
-  const [dbName, setDbName] = useState('your_db_name')
-  const { connectDb } = useDb()
-  const [db, setDb] = useState(null)
-  const [queryResult, setQueryResult] = useState('')
-
-  async function select() {
-    const result = await db.select('SELECT * FROM clients LIMIT 1')
-    setQueryResult(JSON.stringify(result))
-  }
-
-  const connect = async () => {
-    const db = await connectDb(`postgres://postgres:@${host}/${dbName}`)
-    setDb(db)
-  }
-
-  return (
-    <div className="container">
-      <h1>Welcome to Tauri!!</h1>
-      <div className="row">
-        <div>
-          <label>Host</label>
-          <input
-            type="text"
-            value={host}
-            onChange={(e) => setHost(e.target.value)}
-          />
-        </div>
-      </div>
-      <div className="row">
-        <div>
-          <label>DB Name</label>
-          <input
-            type="text"
-            value={dbName}
-            onChange={(e) => setDbName(e.target.value)}
-          />
-        </div>
-      </div>
-      <div className="row">
-        <div>
-          <button type="button" onClick={() => connect()}>
-            Connect
-          </button>
-        </div>
-      </div>
-      {db && (
-        <>
-          <div className="row">
-            <div>
-              <button type="button" onClick={() => select()}>
-                Query
-              </button>
-            </div>
-          </div>
-          <div className="row">
-            <textarea
-              value={queryResult}
-              readOnly
-              style={{
-                width: '300px',
-                height: '150px'
-              }}
-            />
-          </div>
-        </>
-      )}
-    </div>
-  )
+  return <div></div>
 }
 
 export default App
